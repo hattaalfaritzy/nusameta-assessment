2f89c357db0b9c45d516777d7f032b9a13d25eb1
hattaalfaritzy <muh.hara16@gmail.com>
2023-09-17 06:53:45 +0700

    chores: add more env n fix query fetch

--- src/utils/query-fetch.ts
+++ src/utils/query-fetch.ts
@@ -3,7 +3,7 @@ import { fetchApi } from './api';
 
 export async function queryFetch(_key: string, url = '', params = {}, method: 'GET' | 'POST' = 'GET', data?: Record<string, unknown>) {
     return fetchApi({
-        baseUrl: 'http://localhost:3000/api/',
+        baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/api/',
         method: method,
         url: url,
         params: params,
