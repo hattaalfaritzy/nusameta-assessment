e99843ca20520b2c9d5a25f80902475f8b497acd
hattaalfaritzy <muh.hara16@gmail.com>
2023-09-17 07:13:51 +0700

    fix

--- src/utils/query-fetch.ts
+++ src/utils/query-fetch.ts
@@ -2,8 +2,14 @@
 import { fetchApi } from './api';
 
 export async function queryFetch(_key: string, url = '', params = {}, method: 'GET' | 'POST' = 'GET', data?: Record<string, unknown>) {
+    const isDevelopment = process.env.NODE_ENV === 'development';
+    
+    const baseUrl = isDevelopment
+        ? 'http://localhost:3000/api/'
+        : process.env.NEXT_PUBLIC_BASE_URL;
+
     return fetchApi({
-        baseUrl: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/api/',
+        baseUrl: baseUrl,
         method: method,
         url: url,
         params: params,
